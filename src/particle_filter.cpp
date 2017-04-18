87b89610356d3678f5404408915c780eb4fa9372
kdomnitser <sirpsycho85@gmail.com>
2017-04-17 23:32:50 -0700

    gibberish

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -99,17 +99,73 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	observations is actual measurements from lidar
 	perform nearest neighbor with those inputs
 		so you can assign the landmark ID to the actual measurement
+	now you have for each particle, a mapping between actual measurements and landmarks
+		how close these are corresponds to the weight you give each particle
 
 	for every actual measurement from lidar
 		find the predicted that's closest
 		assign the landmark id from the predicted to the actual measurement
 
 	*/
+	double min_dist, current_dist;
+	int nearest_landmark_id;
+
+	for(int i = 0; i < observations.size(); ++i) {
+		
+		min_dist = dist(observations[i].x, observations[i].y, predicted[0].x, predicted[0].y);
+		nearest_landmark_id = predicted[0].id;
+		
+		for(int j = 1; j < predicted.size(); ++j) {
+			current_dist = dist(observations[i].x, observations[i].y, predicted[j].x, predicted[j].y);
+			if(current_dist < min_dist) {
+				nearest_landmark_id = predicted[j].id;
+			}
+		}
+
+		observations[i].id = nearest_landmark_id;
+	}
 
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
 		std::vector<LandmarkObs> observations, Map map_landmarks) {
+	
+	std::vector<LandmarkObs> predicted;
+
+	vector<Map::single_landmark_s> landmark_list = map_landmarks.landmark_list;
+
+	for(int p_num = 0; p_num < num_particles; ++p_num) {
+		//translation
+		double p_x = particles[p_num].x;
+		double p_y = particles[p_num].y;
+		double p_theta = particles[p_num].x;
+
+		for(int o_num = 0; o_num < observations.size(); ++o_num) {
+			double o_x = observations[o_num].x;
+			double o_y = observations[o_num].y;
+			
+			double temp_x = o_x*cos(p_theta) + o_y*sin(p_theta) + p_x;
+			double temp_y = o_x*sin(p_theta) + o_y*cos(p_theta) + p_y;
+			
+			observations[o_num].x = temp_x;
+			observations[o_num].y = temp_y;
+		}
+
+		//for each landmark...
+		for(int lm_num = 0; lm_num < landmark_list.size(); ++lm_num) {
+			double lm_dist = dist(p_x, p_y, landmark_list[lm_num].x_f, landmark_list[lm_num].y_f);
+			if(lm_dist < sensor_range) {
+				struct LandmarkObs predicted_landmark;
+
+				//TODO - need to add measurements not absolute positions right?
+				predicted_landmark.x = landmark_list[lm_num].x_f;
+				predicted_landmark.y = landmark_list[lm_num].y_f;
+				predicted_landmark.id = landmark_list[lm_num].id_i;
+				
+				predicted.push_back(predicted_landmark);
+			}
+		}
+	}
 
 	// TODO: Update the weights of each particle using a mult-variate Gaussian distribution. You can read
 	//   more about this distribution here: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
@@ -122,6 +178,26 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33. Note that you'll need to switch the minus sign in that equation to a plus to account 
 	//   for the fact that the map's y-axis actually points downwards.)
 	//   http://planning.cs.uiuc.edu/node99.html
+
+	/*
+	For each particle
+		Transform observations into map coordiante system
+		For each landmark
+			Calculate distance to each actual landmark (predict measurements)
+			If it's within sensor_range, add as a LandmarkObs to 'predicted'
+		Call data association to update measurements with landmark IDs
+		Update weights with multi-variate Gaussian
+			Note: The same landmark within range may be the nearest for a measurement
+			Note: Might be convenient in dataAssociation to create a new vector with actual landmarks
+			That way you don't have to search through them again
+		Normalize weights
+	*/
+
+	// TODO: handle empty 'predicted' or 'observations'
+	// TODO: how do you know the map y points downward?
+	// TODO: rotation matrix in article disagrees with wiki
+	// TODO: not clear if the transformation is correct. See what happens to x for a measurement after translating, given some theta for a particle
+	// 			also distances seem large
 }
 
 void ParticleFilter::resample() {
